9040ed7b87f082865d3c4018898e198b5930b47c
ilknur-kara <ilknur.dkara@gmail.com>
2022-06-19 22:52:19 +0300

    Added the project

---
+++ main.py
@@ -0,0 +1,111 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+
+class Button:
+    def __init__(self, pos, width, height, value):
+        self.pos = pos
+        self.width = width
+        self.height = height
+        self.value = value
+
+    def draw(self, img):
+        cv2.rectangle(img, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
+                      (225, 225, 225), cv2.FILLED)
+        cv2.rectangle(img, self.pos, (self.pos[0] + self.width, self.pos[1] + self.height),
+                      (50, 50, 50), 3)
+        cv2.putText(img, self.value, (self.pos[0] + 2, self.pos[1] + 50), cv2.FONT_HERSHEY_PLAIN,
+                    2, (50, 50, 50), 2)
+
+    def checkClick(self, x, y): # clikc fonksiyonu burası. Belirlenen mesafe 50 iki parmak arası 50 uzunluğu altındaysa ilk if kısmına giriyor
+        if self.pos[0] < x < self.pos[0] + self.width and \
+                self.pos[1] < y < self.pos[1] + self.height:
+            cv2.rectangle(img, (self.pos[0] + 3, self.pos[1] + 3),
+                          (self.pos[0] + self.width - 3, self.pos[1] + self.height - 3),
+                          (255, 255, 255), cv2.FILLED)
+            cv2.putText(img, self.value, (self.pos[0] + 25, self.pos[1] + 80), cv2.FONT_HERSHEY_PLAIN,
+                        5, (0, 0, 0), 5)
+            return True
+        else:
+            return False
+
+#WebCam
+cap = cv2.VideoCapture(0)
+cap.set(3, 1288) #width
+cap.set(4, 720) #height
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+#Create Button
+buttonListValues = [['Filter Coffee', 'Americano'],
+                    ['Latte', 'Long Black'],
+                    ['Guillermo', 'Frappe'],
+                    ['Espresso', 'Cappuccino'],
+                    ['Continue', 'Pay it']]
+buttonList = []
+for x in range(2):
+    for y in range(5):
+        xpos = x * 280 + 350
+        ypos = y * 80 + 200
+
+        buttonList.append(Button((xpos, ypos), 300, 80, buttonListValues[y][x]))
+
+#Variables
+myChoice = ''
+delayCounter = 0 #çoklu tıklamayı önlemek için sayaç oluşturduk
+
+#Loop
+while True:
+    #Get image from webcam
+    success, img = cap.read()
+    img = cv2.flip(img, 1) #kamerayı döndürüyor.
+
+    #Detection hand
+    hands, img = detector.findHands(img, flipType=False)
+
+    #Drawing top level rectangular
+    # cv2.rectangle(img, (600, 80), (600 + 580, 50 + 100),
+    #               (225, 225, 225), cv2.FILLED)
+    #
+    # cv2.rectangle(img, (600, 80), (600 + 580, 50 + 100),
+    #               (50, 50, 50), 3)
+
+    for button in buttonList:
+        button.draw(img)
+
+    #Finger Click
+    if hands:
+        lmList = hands[0]['lmList']
+        length, _, img = detector.findDistance(lmList[8], lmList[12], img) #burada 8numaralı ve 12numaralı noktaları alıyoruz ve uzunluğunu ölçüyoruz
+        print(length)
+        x, y = lmList[8]
+        if length < 50 and delayCounter == 0:
+            for i, button in enumerate(buttonList):
+                if button.checkClick(x, y):
+                    myValue = buttonListValues[int(i % 5)][int(i / 5)]  # get correct number
+                    if myValue == 'Continue': #örnekte burada eşittire basılırsa mat işlemi yapıyor
+                        myChoice = 'To continue, press payment button.' #eval komutu matematik işlemi yapıyor bizim sıfırlama olayını yapacağımız yer burası
+                    elif myValue == 'Pay it':
+                        myChoice = 'Your payment is accepted.'
+                    else:
+                        myChoice = myValue + ' is chosen.'
+                    delayCounter = 1
+
+#multi-click prevention
+    if delayCounter != 0:
+        delayCounter += 1
+        if delayCounter > 10:
+            delayCounter = 0
+
+    #Display the Result
+    cv2.putText(img, myChoice, (345, 110), cv2.FONT_HERSHEY_PLAIN,
+                2, (80, 50, 80), 3)
+
+    #Display Image
+    key = cv2.waitKey(1)
+    cv2.imshow("Virtual Coffee Vending Machine", img)
+    #cv2.waitKey(1)
+    if key == ord('c'): #burada klavyede c tuşuna basınca ekran sıfırlanıyor
+        myChoice = ''
+
+
+
